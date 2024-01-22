814d2d31bb1382a493340f4cb14a511fa2c6f6a3
Yazan Tarifi <yazantarifi989@gmail.com>
2024-01-22 20:00:30 +0300

    Build Not Found Page

--- src/pages/not.found.page.component.jsx
+++ src/pages/not.found.page.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ScreenContainerComponent from "../common/screen.container.component";
 import ToolbarComponents from "../common/toolbar.components";
+import AppFooterComponent from "../views/app.footer.component";
 
 export default function NotFoundPageComponent() {
     return (
         <ScreenContainerComponent title={"Tapper - Page Not Found"}>
-            <ToolbarComponents />
-            <h1>Not Found</h1>
+           <main>
+               <ToolbarComponents />
+               <div className={"notFoundContent flex items-center justify-between mx-auto"}>
+                   <div className={"p-4 mx-auto"} style={{ textAlign: "center" }}>
+                       <h1><strong>Content Not Found</strong></h1>
+                       <h1>The Page You are searching for Not Found, Try Another Link Please ...</h1>
+                   </div>
+               </div>
+               <AppFooterComponent />
+           </main>
         </ScreenContainerComponent>
     );
 }
